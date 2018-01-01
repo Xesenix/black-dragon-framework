86801d0f2f20535cea3fb75fde520febb042e92b
Xesenix <pkapalla@xesenix.pl>
2018-01-01 23:24:57 +0100

    BDF00 chore(package) - update and add karma modules

--- karma.conf.js
+++ karma.conf.js
@@ -42,7 +42,8 @@ module.exports = function (config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-			'./src/**/*.ts': ['webpack']
+			// './src/**/*.spec.ts': ['webpack', 'sourcemap'],
+			'./src/main.test.ts': ['webpack', 'coverage', 'sourcemap'],
 		},
 
 
@@ -52,7 +53,65 @@ module.exports = function (config) {
 		// test results reporter to use
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['progress'],
+		reporters: [
+			'progress',
+			// 'html',
+			'kjhtml',
+			// 'coverage-istanbul',
+			// 'istanbul',
+			// 'remap-coverage'
+		],
+
+
+		htmlReporter: {
+			outputFile: 'result.html',
+			outputDir: './logs/tests',
+			// Optional
+			pageTitle: 'BDF Tests',
+			subPageTitle: 'unit test results',
+			groupSuites: true,
+			useCompactStyle: false,
+			useLegacyStyle: false,
+
+			templatePath: null, // set if you moved jasmine_template.html
+			focusOnFailures: true, // reports show failures on start
+			namedFiles: false, // name files instead of creating sub-directories
+			urlFriendlyName: true, // simply replaces spaces with _ for files/dirs
+			reportName: 'report-summary-filename', // report summary filename; browser info by default
+
+			// experimental
+			preserveDescribeNesting: true, // folded suites stay folded
+			foldAll: true // reports start folded (only with preserveDescribeNesting)
+		},
+
+
+		coverageReporter: {
+			// dir: 'logs/coverage',
+			reporters: [
+				{ type: 'in-memory' },
+				{ type: 'lcov', subdir: '.' },
+				// { type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
+				// { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+				// { type: 'text', subdir: '.', file: 'coverage.txt' },
+				// { type: 'html', subdir: './html' }
+			]
+		},
+
+		remapCoverageReporter: {
+			'text-summary': null,
+			json: './coverage/coverage.json',
+			html: './coverage/html',
+			lcovonly: './coverage/lcov.info'
+		},
+
+		coverageIstanbulReporter: {
+			reports: [ 'text-summary' ],
+			fixWebpackSourcePaths: true
+		},
+
+		webpackMiddleware: {
+			stats: 'errors-only'
+		},
 
 
 		// web server port
@@ -65,7 +124,7 @@ module.exports = function (config) {
 
 		// level of logging
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: config.LOG_INFO,
+		logLevel: config.LOG_DEBUG,
 
 
 		// enable / disable watching file and executing tests whenever any file changes
